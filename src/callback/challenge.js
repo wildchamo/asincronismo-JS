e72bbeab1fb153b7bf40b32c25605f602a4c40be
wild.chamo <joselitobedoya230@gmail.com>
2021-06-17 18:35:15 -0500

    primera llamada a un api

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,5 +1,5 @@
-let XMLHttpRequest =required('xmlhttprequest').XMLHttpRequest;
-
+let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const api= 'https://rickandmortyapi.com/api/character/';
 function fetchData(url_api,callback){
     let xhttp= new XMLHttpRequest();
     xhttp.open('GET',url_api,true); //activar el asincronismo
@@ -15,3 +15,19 @@ function fetchData(url_api,callback){
     }
     xhttp.send();
 }
+
+fetchData(api,function(error1,data1){
+    if(error1) return console.error(error1);
+    fetchData(api +data1.results[0].id,function(error2,data2){
+        if(error2) return console.error(error2);
+        fetchData(data2.origin.url,function(error3,data3){
+            if(error3) return console.error(error3);
+            console.log(data1.info.count);
+            console.log(data2.name);
+            console.log(data3.dimension);
+        });
+    })
+})
+
+
+
