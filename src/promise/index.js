0424e5c6edc3846986dcce62b27628015a4c905f
wild.chamo <joselitobedoya230@gmail.com>
2021-06-19 10:10:24 -0500

    ejecutar mas de una promesa a la vez y sacar un arreglo como respuesta

--- src/promise/index.js
+++ src/promise/index.js
@@ -12,3 +12,34 @@ const somethingWillHappend = () =>{
 somethingWillHappend ()
  .then(response=> console.log(response))
  .catch(err=>console.log(err))
+
+
+
+const somethingWillHappend2 = () =>{
+    return new Promise((resolve,reject)=>{
+        if(true){
+            setTimeout(()=>{
+                resolve('true');
+            },2000)
+        }else{
+            const error= new Error('paila');
+            reject(error);
+        }
+    });
+}
+
+ somethingWillHappend2 ()
+  .then(response=> console.log(response))
+  .catch(err => console.error(err))
+
+
+
+  //correr promesas al mismo tiempo, retornar un arreglo con los resultados
+   
+  Promise.all([somethingWillHappend(),somethingWillHappend2()])
+    .then(response =>{
+        console.log('array or results',response)
+    })
+    .catch(err =>{
+        console.error(err);
+    })
